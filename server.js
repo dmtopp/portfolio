c70024fbfdb39cc71266d091f9b1051ef32b31c1
Daniel Topp <dmtopp@gmail.com>
2016-05-10 18:10:24 -0500

    changed port for heroku

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res, next) {
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   console.log('The server is listening on port ' + server.address().port);
 })
